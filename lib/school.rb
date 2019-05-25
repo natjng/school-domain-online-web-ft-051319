f4e5853546d6b523058d08f2287534819e26e979
Natalie J <natsetc@gmail.com>
2019-05-25 04:19:33 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -14,7 +14,8 @@ class School
     #   @roster[grade] << student_name
     # end
     
-    @roster[grade] == nil ? @roster[grade] = [name] : @roster[grade] << name
+    roster[grade] ||= []
+    roster[grade] << student_name
   end
   
   def grade(grade)
